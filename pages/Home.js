d707b987ad1568ec2969e764ded70c7fdf9203d5
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-05-04 19:50:46 -0400

    Added files which will store the pages

---
+++ pages/Home.js
@@ -0,0 +1,14 @@
+// This is the main page for the application where recording may take places.
+
+import React from "react";
+import { View } from "react-native";
+
+const Home = ({ navigation }) => {
+  return (
+    <View>
+      <></>
+    </View>
+  );
+};
+
+export default Home;
---
+++ pages/Speech.js
@@ -0,0 +1,13 @@
+// This is where the transcribed speech files will reside, and be accessed from
+import React from "react";
+import { View } from "react-native";
+
+const Speech = ({ navigation }) => {
+  return (
+    <View>
+      <></>
+    </View>
+  );
+};
+
+export default Speech;
