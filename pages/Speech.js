4f037603f4c87756e68812946e8e0cf6b5b9b3cd
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-05-04 20:42:04 -0400

    Added flex to scrollview. Idk why, but I did :D

--- pages/Speech.js
+++ pages/Speech.js
@@ -7,7 +7,7 @@ const Speech = ({ navigation }) => {
   const [files, setFiles] = useState([]);
 
   return (
-    <ScrollView>
+    <ScrollView style={{ flex: 1 }}>
       <></>
       {/* Go through each file in Files, and then display them or something ig... */}
       <File name="Testing" preview="These are the first few words of the..." />
