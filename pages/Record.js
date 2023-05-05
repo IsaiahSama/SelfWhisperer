0e788e7f0ded70e7c22f6b2a8d261728c60ba4cf
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-05-04 20:24:10 -0400

    Fixed issue with navigation. Will now change icon on press

--- pages/Record.js
+++ pages/Record.js
@@ -1,6 +1,6 @@
 // This is the main page for the application where recording may take places.
 
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -11,18 +11,34 @@ import {
 } from "react-native";
 
 const Record = ({ navigation }) => {
-  const doSomething = () => {};
+  const [recording, setRecording] = useState(false);
+
+  const doSomething = () => {
+    setRecording(!recording);
+  };
 
   return (
     <View style={styles.container}>
       <TouchableOpacity onPress={doSomething}>
-        <Image source={require("../assets/mic_off.png")} style={styles.image} />
+        {recording ? (
+          <Image
+            source={require("../assets/mic_on.png")}
+            style={styles.image}
+          />
+        ) : (
+          <Image
+            source={require("../assets/mic_off.png")}
+            style={styles.image}
+          />
+        )}
       </TouchableOpacity>
       <View style={styles.spacer} />
       <Text>Press me to start recording</Text>
       <View style={styles.spacer} />
       <Button
-        onPress={navigation.navigate("Files")}
+        onPress={() => {
+          navigation.navigate("Files");
+        }}
         title="Go to Transcriptions"
       />
     </View>
