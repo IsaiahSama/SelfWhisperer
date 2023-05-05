18b67046181388ab98d5e1906a00adaf5f93850c
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-05-04 20:32:15 -0400

    Added dummy data to preview

--- pages/Speech.js
+++ pages/Speech.js
@@ -1,6 +1,7 @@
 // This is where the transcribed speech files will reside, and be accessed from
 import React, { useState } from "react";
 import { ScrollView } from "react-native";
+import File from "../components/File";
 
 const Speech = ({ navigation }) => {
   const [files, setFiles] = useState([]);
@@ -9,6 +10,11 @@ const Speech = ({ navigation }) => {
     <ScrollView>
       <></>
       {/* Go through each file in Files, and then display them or something ig... */}
+      <File name="Testing" preview="These are the first few words of the..." />
+      <File
+        name="Another Test"
+        preview="Somebody once told me the world was mac..."
+      />
     </ScrollView>
   );
 };
