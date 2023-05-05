8ef3fb0ed6cb3649546b8e9ae21433625006a77d
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-05-04 20:28:53 -0400

    Added file component to represent a singular file

---
+++ components/File.js
@@ -0,0 +1,12 @@
+// Represents a singular file
+
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const File = () => {
+  return (
+    <View>
+      <></>
+    </View>
+  );
+};
