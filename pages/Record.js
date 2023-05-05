e1c6467c29636ee9ab94acc70001ba08b8584cfa
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-05-04 20:04:21 -0400

    Added images and updated code

--- pages/Record.js
+++ pages/Record.js
@@ -1,14 +1,29 @@
 // This is the main page for the application where recording may take places.
 
 import React from "react";
-import { View } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 const Record = ({ navigation }) => {
   return (
-    <View>
-      <></>
+    <View style={styles.container}>
+      <Image source={require("../assets/mic_off.png")} style={styles.image} />
+      <Text>Press me to start recording</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  image: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+  },
+});
 
 export default Record;
