39763afa88c41025242aa2329de7cdec9911af52
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-05-04 21:18:32 -0400

    Added styling for File Components

--- components/File.js
+++ components/File.js
@@ -1,14 +1,30 @@
 // Represents a singular file
 
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 
 const File = ({ name, preview }) => {
   return (
-    <View>
-      <></>
+    <View style={styles.card}>
+      <Text>Name: {name}</Text>
+      <Text>Preview: {preview}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  card: {
+    display: "flex",
+    flex: 1,
+    justifyContent: "space-between",
+    maxHeight: 90,
+    height: 70,
+    marginBottom: 10,
+    backgroundColor: "#f9f9f9",
+    borderRadius: 5,
+    padding: 10,
+    elevation: 10,
+  },
+});
 
 export default File;
