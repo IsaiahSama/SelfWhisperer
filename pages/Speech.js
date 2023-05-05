14e2d1870d659b35f633c57caddfbb4196c7744c
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-05-04 20:45:36 -0400

    Margin :o

--- pages/Speech.js
+++ pages/Speech.js
@@ -7,7 +7,7 @@ const Speech = ({ navigation }) => {
   const [files, setFiles] = useState([]);
 
   return (
-    <ScrollView style={{ flex: 1 }}>
+    <ScrollView style={{ flex: 1, marginLeft: 5 }}>
       <></>
       {/* Go through each file in Files, and then display them or something ig... */}
       <File name="Testing" preview="These are the first few words of the..." />
