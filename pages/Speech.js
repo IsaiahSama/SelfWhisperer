59deed9ebb2235ef991573a9c6651bfaf1a13b7b
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-05-05 21:28:42 -0400

    Will now read the keys from AsyncStorage and store them in the Files state

--- pages/Speech.js
+++ pages/Speech.js
@@ -1,10 +1,26 @@
 // This is where the transcribed speech files will reside, and be accessed from
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ScrollView } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 import File from "../components/File";
 
 const Speech = ({ navigation }) => {
   const [files, setFiles] = useState([]);
+
+  useEffect(async () => {
+    let keys = [];
+    try {
+      keys = await AsyncStorage.getAllKeys();
+    } catch (e) {
+      console.error(e);
+    }
+
+    for (let key of keys) {
+      let value = await AsyncStorage.getItem(key);
+      setFiles([...files, { name: key, text: value }]);
+    }
+  }, []);
 
   return (
     <ScrollView style={{ flex: 1, marginLeft: 5 }}>
