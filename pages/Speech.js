84b811f3343942e83236c2e048791c09d060a260
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-05-05 21:36:24 -0400

    Now displays transcriptions.

--- pages/Speech.js
+++ pages/Speech.js
@@ -8,34 +8,37 @@ import File from "../components/File";
 const Speech = ({ navigation }) => {
   const [files, setFiles] = useState([]);
 
-  useEffect(async () => {
-    let keys = [];
-    try {
-      keys = await AsyncStorage.getAllKeys();
-    } catch (e) {
-      console.error(e);
-    }
+  useEffect(() => {
+    const handleFiles = async () => {
+      let keys = [];
+      try {
+        keys = await AsyncStorage.getAllKeys();
+      } catch (e) {
+        console.error(e);
+      }
 
-    for (let key of keys) {
-      let value = await AsyncStorage.getItem(key);
-      setFiles([...files, { name: key, text: value }]);
-    }
+      for (let key of keys) {
+        let value = await AsyncStorage.getItem(key);
+        setFiles([...files, { name: key, text: value }]);
+        // await AsyncStorage.removeItem(key);
+      }
+    };
+
+    handleFiles();
   }, []);
 
   return (
     <ScrollView style={{ flex: 1, marginLeft: 5 }}>
-      <></>
       {/* Go through each file in Files, and then display them or something ig... */}
-      <File
-        name="Testing"
-        preview="These are the first few words of the..."
-        link={"Something"}
-      />
+      <File name="Testing" text="These are the first few words of the..." />
       <File
         name="Another Test"
-        preview="Somebody once told me the world was mac..."
-        link="Somewhere"
+        text="Somebody once told me the world was mac..."
       />
+
+      {files.map((file) => (
+        <File key={file.name} name={file.name} text={file.text} />
+      ))}
     </ScrollView>
   );
 };
